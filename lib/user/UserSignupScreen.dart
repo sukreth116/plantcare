c3794bcaa574ccb9d270ddc3712482b30014d8e9
Sukreth O M <sukrethom2000@gmail.com>
2025-01-22 15:55:18 +0530

    first commit

---
+++ lib/user/UserSignupScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class UserSignupScreen extends StatefulWidget {
+  const UserSignupScreen({super.key});
+
+  @override
+  State<UserSignupScreen> createState() => _UserSignupScreenState();
+}
+
+class _UserSignupScreenState extends State<UserSignupScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(backgroundColor: Colors.green,
+    title: Text('Plant Care'),),
+    body: Column(children: [ Text("Name",style: TextStyle(fontSize: 20),),TextFormField(decoration: InputDecoration(hintText: "Enter Your Name"),)],),);
+  }
+}
