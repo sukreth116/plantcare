85af8216cfb31139b7190f34db4ff6123a6f5ad5
Sukreth O M <sukrethom2000@gmail.com>
2025-01-22 21:29:50 +0530

    second change

--- lib/user/UserSignupScreen.dart
+++ lib/user/UserSignupScreen.dart
@@ -12,6 +12,6 @@ class _UserSignupScreenState extends State<UserSignupScreen> {
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(backgroundColor: Colors.green,
     title: Text('Plant Care'),),
-    body: Column(children: [ Text("Name",style: TextStyle(fontSize: 20),),TextFormField(decoration: InputDecoration(hintText: "Enter Your Name"),)],),);
+    body: Column(children: [ Text("First Name",style: TextStyle(fontSize: 20),),TextFormField(decoration: InputDecoration(hintText: "Enter Your Name"),)],),);
   }
 }
