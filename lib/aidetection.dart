de6baed0c8bc7f0e8e7a2232c7cc7bca495e6f98
Sukreth O M <sukrethom2000@gmail.com>
2025-05-30 23:34:55 +0530

    ai second

--- lib/aidetection.dart
+++ lib/aidetection.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
-
 import 'dart:convert';
 import 'package:url_launcher/url_launcher.dart';
 
